aa75e651614d6d1571155cd558fd4a3226c68383
bchavet <ben@chavet.net>
2008-02-22 04:18:30 +0000

    things and stuff
    git-svn-id: file:///mnt/media/backups/pokey/2011-03-05/svn/sheepserver/trunk@3 36b1bf71-1d55-412a-8d7b-506455af119f

--- tinymvc/myapp/controllers/default.php
+++ tinymvc/myapp/controllers/default.php
@@ -1,20 +1,34 @@
 <?php
-
 /**
  * default.php
  *
  * default application controller
- *
- * @package		TinyMVC
- * @author		Monte Ohrt
  */
 
 class Default_Controller extends TinyMVC_Controller
 {
-  function index()
-  {
-    $this->view->display('index_view');
-  }
-}
+    function index()
+    {
+        if (file_exists(TMVC_MYAPPDIR . 'configs' . DS . 'database.php')) {
 
-?>
+            if (!strncmp(PHP_SAPI, 'cgi', 3)) {
+                // When using CGI PHP, SCRIPT_NAME may contain the path to
+                // the PHP binary instead of the script being run; use
+                // PHP_SELF instead.
+                $url = $_SERVER['PHP_SELF'];
+            } else {
+                $url = isset($_SERVER['SCRIPT_NAME']) ?
+                    $_SERVER['SCRIPT_NAME'] :
+                    $_SERVER['PHP_SELF'];
+            }
+
+            //TODO: Check that the apache redirects are in place somehow
+
+            header('Location: ' . $url . '/cgi');
+            exit;
+
+        } else {
+            $this->view->display('index_view');
+        }
+    }
+}
