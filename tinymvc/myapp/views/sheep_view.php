629cb85ed2470aaada1849719967eea8cc6c9782
bchavet <ben@chavet.net>
2008-03-15 22:20:50 +0000

    re-add incomplete frame placeholder
    git-svn-id: file:///mnt/media/backups/pokey/2011-03-05/svn/sheepserver/trunk@23 36b1bf71-1d55-412a-8d7b-506455af119f

--- tinymvc/myapp/views/sheep_view.php
+++ tinymvc/myapp/views/sheep_view.php
@@ -13,7 +13,9 @@ foreach ($frames as $frame) {
      echo '<a href="/status?sheep=' . $frame['sheep_id'] . '&amp;frame=' . $frame['frame_id'] . '">';
      if (file_exists(ES_BASEDIR . DS . 'gen' . DS . $frame['flock_id'] . DS . $frame['sheep_id'] . DS . $frame['frame_id'] . '.thumbnail.jpg')) {
          echo '<img src="/gen/' . $frame['flock_id'] . '/' . $frame['sheep_id'] . '/' . $frame['frame_id'] . '.thumbnail.jpg" alt="" class="thumbnail" />';
-     }         
+     } else {
+         echo '<img src="/images/anon-icon.jpg" alt="" class="thumbnail" />';
+     }
      echo '</a>';
  }
 ?>
