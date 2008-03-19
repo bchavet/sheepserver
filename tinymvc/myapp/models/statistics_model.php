a7feb5ae44a63c8b3fade99af33ba14b958bbfb2
bchavet <ben@chavet.net>
2008-03-19 05:41:14 +0000

    clean up "assigned" stats
    git-svn-id: file:///mnt/media/backups/pokey/2011-03-05/svn/sheepserver/trunk@38 36b1bf71-1d55-412a-8d7b-506455af119f

--- tinymvc/myapp/models/statistics_model.php
+++ tinymvc/myapp/models/statistics_model.php
@@ -17,7 +17,7 @@ class Statistics_Model extends TinyMVC_Model
 
     function getAssignedFrames($flock)
     {
-        return $this->db->query_all('select * from frame where flock_id=? and state=? order by sheep_id, frame_id asc',
+        return $this->db->query_all('select * from sheep, frame where frame.flock_id=? and frame.state=? and sheep.sheep_id=frame.sheep_id order by frame.sheep_id, frame.frame_id asc',
                                     array($flock, 'assigned'));
     }
 }
