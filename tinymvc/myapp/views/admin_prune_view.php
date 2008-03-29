e2de995139648271ca92f5a342da3c29044ae519
bchavet <ben@chavet.net>
2008-03-29 04:58:12 +0000

    Show confirmation page after pruning
    git-svn-id: file:///mnt/media/backups/pokey/2011-03-05/svn/sheepserver/trunk@79 36b1bf71-1d55-412a-8d7b-506455af119f

--- tinymvc/myapp/views/admin_prune_view.php
+++ tinymvc/myapp/views/admin_prune_view.php
@@ -20,44 +20,47 @@
 </div>
 
 <h2>Prune</h2>
+<?php if (!$confirm): ?>
 <p>The following sheep and edges connecting them will be pruned</p>
 
 <table>
 <?php
 foreach ($prunelist as $prune) {
-     echo '<tr>';
-     if (!$confirm) {
-         echo '<td><a href="/sheep/status?sheep=' . $prune['sheep_id'] . '">';
-         if ($prune['first'] != $prune['last']) {
-
-             if (file_exists(ES_BASEDIR . DS . 'gen' . DS . $prune['flock_id'] . DS . $prune['first'] . DS . '0.thumbnail.jpg')) {
-                 echo '<img src="/gen/' . $prune['flock_id'] . '/' . $prune['first'] . '/0.thumbnail.jpg" alt="" class="thumbnail edgefirst" />';
-             } else {
-                 echo '<img src="/images/anon-icon.jpg" alt="" class="thumbnail edgefirst" />';
-             }
-             if (file_exists(ES_BASEDIR . DS . 'gen' . DS . $prune['flock_id'] . DS . $prune['last'] . DS . '0.thumbnail.jpg')) {
-                 echo '<img src="/gen/' . $prune['flock_id'] . '/' . $prune['last'] . '/0.thumbnail.jpg" alt="" class="thumbnail edgelast" />';
-             } else {
-                 echo '<img src="/images/anon-icon.jpg" alt="" class="thumbnail edgelast" />';
-             }
-
+     echo '<tr><td><a href="/sheep/status?sheep=' . $prune['sheep_id'] . '">';
+     if ($prune['first'] != $prune['last']) {
+         
+         if (file_exists(ES_BASEDIR . DS . 'gen' . DS . $prune['flock_id'] . DS . $prune['first'] . DS . '0.thumbnail.jpg')) {
+             echo '<img src="/gen/' . $prune['flock_id'] . '/' . $prune['first'] . '/0.thumbnail.jpg" alt="" class="thumbnail edgefirst" />';
          } else {
-
-             if (file_exists(ES_BASEDIR . DS . 'gen' . DS . $prune['flock_id'] . DS . $prune['sheep_id'] . DS . '0.thumbnail.jpg')) {
-                 echo '<img src="/gen/' . $prune['flock_id'] . '/' . $prune['sheep_id'] . '/0.thumbnail.jpg" alt="" class="thumbnail" />';
-             } else {
-                 echo '<img src="/images/anon-icon.jpg" alt="" class="thumbnail" />';
-             }
-
+             echo '<img src="/images/anon-icon.jpg" alt="" class="thumbnail edgefirst" />';
          }
-         echo '</a></td>';
+         if (file_exists(ES_BASEDIR . DS . 'gen' . DS . $prune['flock_id'] . DS . $prune['last'] . DS . '0.thumbnail.jpg')) {
+             echo '<img src="/gen/' . $prune['flock_id'] . '/' . $prune['last'] . '/0.thumbnail.jpg" alt="" class="thumbnail edgelast" />';
+         } else {
+             echo '<img src="/images/anon-icon.jpg" alt="" class="thumbnail edgelast" />';
+         }
+         
+     } else {
+         
+         if (file_exists(ES_BASEDIR . DS . 'gen' . DS . $prune['flock_id'] . DS . $prune['sheep_id'] . DS . '0.thumbnail.jpg')) {
+             echo '<img src="/gen/' . $prune['flock_id'] . '/' . $prune['sheep_id'] . '/0.thumbnail.jpg" alt="" class="thumbnail" />';
+         } else {
+             echo '<img src="/images/anon-icon.jpg" alt="" class="thumbnail" />';
+         }
+         
      }
-     echo '</tr>';
+     echo '</a></td></tr>';
 }
 ?>
 </table>
 
 <div><a href="/admin/prune?numdays=<?= $numdays ?>&amp;confirm=yes">OK</a></div>
+
+<?php else: ?>
+
+<?= count($prunelist) ?> sheep and their connecting edges have been pruned.
+
+<?php endif; ?>
 
 </body>
 
