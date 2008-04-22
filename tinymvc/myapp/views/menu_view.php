f26a1279b5555eb79f69af37dc015054dc0e7a4c
bchavet <ben@chavet.net>
2008-04-22 06:57:40 +0000

    clean up menu styles a bit
    git-svn-id: file:///mnt/media/backups/pokey/2011-03-05/svn/sheepserver/trunk@107 36b1bf71-1d55-412a-8d7b-506455af119f

--- tinymvc/myapp/views/menu_view.php
+++ tinymvc/myapp/views/menu_view.php
@@ -1,59 +1,73 @@
-<div class="navigation">
-<ul>
-  <li class="first">Flock <?= $flock ?></li>
-  <li><a href="/flock/loops">Loops</a></li>
-  <li><a href="/flock/edges">Edges</a></li>
-  <li><a href="/flock/archive">Archive</a></li>
-  <li><a href="/flock/queue">Queue</a></li>
-  <li><a href="/flock/credit">Credit</a></li>
-  <li><a href="/flock/stats">Statistics</a></li>
-  <?php if (empty($_SESSION['logged_in'])): ?>
-  <li><a href="/admin">Login</a></li>
-  <?php else: ?>
-  <li><a href="/admin">Admin</a></li>
-  <li><a href="/admin/logout">Logout</a></li>
-  <?php endif; ?>
-</ul>
-<div class="clr"></div>
-</div>
+<?php
 
-<?php if (isset($sheep) && is_array($sheep)): ?>
-<div class="navigation">
-<ul>
-  <li class="first">Sheep <?= $sheep['sheep_id'] ?></li>
-  <li><a href="/sheep?sheep=<?= $sheep['sheep_id'] ?>">View</a></li>
-  <?php if ($sheep['state'] != 'archive'): ?>
-  <li><a href="/sheep/frames?sheep=<?= $sheep['sheep_id'] ?>">Frames</a></li>
-  <?php endif; ?>
-  <?php if ($sheep['first'] == $sheep['last']): ?>
-  <li><a href="/sheep/family?sheep=<?= $sheep['sheep_id'] ?>">Family</a></li>
-  <?php endif; ?>
-  <li><a href="/sheep/genome?sheep=<?= $sheep['sheep_id'] ?>">Genome</a></li>
-  <?php if ($sheep['state'] != 'archive'): ?>
-  <li><a href="/sheep/credit?sheep=<?= $sheep['sheep_id'] ?>">Credit</a></li>
-  <?php endif; ?>
-  <?php if (file_exists(ES_BASEDIR . DS . 'gen' . DS . $flock . DS . $sheep['sheep_id'] . DS . 'sheep.mpg')): ?>
-  <li><a href="/gen/<?= $flock ?>/<?= $sheep['sheep_id'] ?>/sheep.mpg">Download</a></li>
-  <?php endif; ?>
-</ul>
-<?php if (!empty($_SESSION['logged_in']) && $sheep['state'] != 'archive'): ?>
-<ul class="admin">
-  <li class="first"><a href="/sheep?sheep=<?= $sheep['sheep_id'] ?>&amp;action=archive">Archive</a></li>
-  <li><a href="/sheep?sheep=<?= $sheep['sheep_id'] ?>&amp;action=delete">Delete</a></li>
-</ul>
-<?php endif; ?>
-<?php if (!empty($_SESSION['logged_in']) && $sheep['state'] == 'archive'): ?>
-<ul class="admin">
-  <li class="first"><a href="/sheep?sheep=<?= $sheep['sheep_id'] ?>&amp;action=unarchive">Unarchive</a></li>
-</ul>
-<?php endif; ?>
-<div class="clr"></div>
-</div>
+// Main Menu
 
-<?php if (!empty($sheep['credit_link'])): ?>
-<div>
-Original Sheep: <a href="<?= $sheep['credit_link'] ?>"><?= $sheep['credit_link'] ?></a>
-</div>
-<?php endif; ?>
+echo '<div class="navigation">';
+echo '<ul>';
+echo '<li class="first">Flock ' . $flock . '</li>';
+echo '<li><a href="/flock/loops">Loops</a></li>';
+echo '<li><a href="/flock/edges">Edges</a></li>';
+echo '<li><a href="/flock/archive">Archive</a></li>';
+echo '<li><a href="/flock/queue">Queue</a></li>';
+echo '<li><a href="/flock/credit">Credit</a></li>';
+echo '<li><a href="/flock/stats">Statistics</a></li>';
 
-<?php endif; ?>
+if (empty($_SESSION['logged_in'])) {
+    echo '<li><a href="/admin">Login</a></li>';
+} else {
+    echo '<li><a href="/admin">Admin</a></li>';
+    echo '<li><a href="/admin/logout">Logout</a></li>';
+}
+
+echo '</ul>';
+echo '</div>';
+
+
+// Sheep Menu
+
+if (isset($sheep) && is_array($sheep)) {
+    echo '<div class="navigation">';
+
+    echo '<ul>';
+    echo '<li class="first">Sheep ' . $sheep['sheep_id'] . '</li>';
+    echo '<li><a href="/sheep?sheep=' . $sheep['sheep_id'] . '">View</a></li>';
+
+    if ($sheep['state'] != 'archive') {
+        echo '<li><a href="/sheep/frames?sheep=' . $sheep['sheep_id'] . '">Frames</a></li>';
+    }
+
+    if ($sheep['first'] == $sheep['last']) {
+        echo '<li><a href="/sheep/family?sheep=' . $sheep['sheep_id'] . '">Family</a></li>';
+    }
+
+    echo '<li><a href="/sheep/genome?sheep=' . $sheep['sheep_id'] . '">Genome</a></li>';
+
+    if ($sheep['state'] != 'archive') {
+        echo '<li><a href="/sheep/credit?sheep=' . $sheep['sheep_id'] . '">Credit</a></li>';
+    }
+
+    if (file_exists(ES_BASEDIR . DS . 'gen' . DS . $flock . DS . $sheep['sheep_id'] . DS . 'sheep.mpg')) {
+        echo '<li><a href="/gen/' . $flock . '/' . $sheep['sheep_id'] . '/sheep.mpg">Download</a></li>';
+    }
+
+    echo '</ul>';
+
+    if (!empty($_SESSION['logged_in']) && $sheep['state'] != 'archive') {
+        echo '<ul class="admin">';
+        echo '<li class="first"><a href="/sheep?sheep=' .  $sheep['sheep_id'] . '&amp;action=archive">Archive</a></li>';
+        echo '<li><a href="/sheep?sheep=' . $sheep['sheep_id'] . '&amp;action=delete">Delete</a></li>';
+        echo '</ul>';
+    }
+
+    if (!empty($_SESSION['logged_in']) && $sheep['state'] == 'archive') {
+        echo '<ul class="admin">';
+        echo '<li class="first"><a href="/sheep?sheep=' . $sheep['sheep_id'] . '&amp;action=unarchive">Unarchive</a></li>';
+        echo '</ul>';
+    }
+
+    echo '</div>';
+
+    if (!empty($sheep['credit_link'])) {
+        echo '<div>Original Sheep: <a href="' . $sheep['credit_link'] . '">' . $sheep['credit_link'] . '</a></div>';
+    }
+}
